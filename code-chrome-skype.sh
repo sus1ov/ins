31efa1b35da6c626f5f61ffd4347d016ab743096
Evgeniy <ckopny@gmail.com>
2019-01-08 20:18:10 +0300

    Update code-chrome-skype.sh

--- code-chrome-skype.sh
+++ code-chrome-skype.sh
@@ -1,18 +1,14 @@
-# install gpg key and vscode
+# install vscode gpg key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-apt-get install -y apt-transport-https
-apt-get update && apt-get install -y code
-
-# install google key and 
+# install google public key
 wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-apt-get update && apt-get install -y google-chrome-stable 
-
-# install key and skype
+# install skype repository key
 wget -qO- https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] https://repo.skype.com/deb stable main" > /etc/apt/sources.list.d/skype-stable.list'
-sudo apt-get update
-sudo apt-get install skype-stable -y 
 
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y code google-chrome-stable skype-stable
