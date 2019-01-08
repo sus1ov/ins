e173497bd9f377ece358875b6305fd7e25b12c58
Evgeniy <ckopny@gmail.com>
2019-01-08 18:51:51 +0300

    Update skype.sh

--- skype.sh
+++ skype.sh
@@ -1,5 +1,5 @@
 # install key and skype
-wgeet -qO- https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+wget -qO- https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] https://repo.skype.com/deb stable main" > /etc/apt/sources.list.d/skypeforlinux.list'
 sudo apt-get update
 sudo apt-get install skypeforlinux -y 
