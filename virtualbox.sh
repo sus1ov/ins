d45df1e3115fcacee1ee3b57c39ba6625aa207aa
Evgeniy <ckopny@gmail.com>
2019-01-08 23:36:46 +0300

    Create virtualbox.sh

---
+++ virtualbox.sh
@@ -0,0 +1,4 @@
+# virtual box
+wget https://download.virtualbox.org/virtualbox/5.2.22/virtualbox-5.2_5.2.22-126460~Ubuntu~bionic_amd64.deb
+apt install ./virtualbox-5.2_5.2.22-126460~Ubuntu~bionic_amd64.deb
+
