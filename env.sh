15fd3344ac509674c235bfbbdd21e7ba85675e8f
Evgeniy <ckopny@gmail.com>
2019-01-13 00:49:58 +0300

    Update and rename code-chrome-skype.sh to env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,6 @@
+# install sublime gpg key
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
 # install vscode gpg key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -11,4 +14,8 @@ sh -c 'echo "deb [arch=amd64] https://repo.skype.com/deb stable main" > /etc/apt
 
 apt-get install -y apt-transport-https
 apt-get update
-apt-get install -y code google-chrome-stable skype-stable
+apt-get install -y code \
+                   p7zip-full \
+                   skype-stable \
+                   sublime-text \
+                   google-chrome-stable 
