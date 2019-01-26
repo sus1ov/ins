f0b6e10b0c5289183c22185c1e5aaa8781e61b4a
Evgeniy <ckopny@gmail.com>
2019-01-26 15:42:23 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,15 +1,17 @@
+# yadnex-disk install
+echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install -y yandex-disk
 # install sublime gpg key
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
 # install vscode gpg key
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 # install google public key
-wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 # install skype repository key
-wget -qO- https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+wget -qO- https://repo.skype.com/data/SKYPE-GPG-KEY | apt-key add -
 sh -c 'echo "deb [arch=amd64] https://repo.skype.com/deb stable main" > /etc/apt/sources.list.d/skype-stable.list'
 
 apt-get install -y apt-transport-https vlc 
