b70747f64d498f8a9faa0cea6139bc75327a1b55
Evgeniy <ckopny@gmail.com>
2019-07-24 17:07:07 +0300

    Create alarm_for_sitting.py

---
+++ alarm_for_sitting.py
@@ -0,0 +1,12 @@
+from tkinter import Tk, Button
+period=600000
+def clicked():
+    window.withdraw()
+    window.after(period)
+    window.update()
+    window.deiconify()
+window = Tk()
+window.title("I'm watching you and notify every " + str(period//60000) + " seconds!" )
+btn = Button(window, text="change position", font=("Arial Bold", 50), command = clicked)
+btn.grid(column=0, row=0)
+window.mainloop()
