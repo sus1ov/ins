7741083e0714f9b41c159297c663358eda03dda5
Evgeniy <ckopny@gmail.com>
2019-07-26 17:05:47 +0300

    Update alarm_for_sitting.py

--- alarm_for_sitting.py
+++ alarm_for_sitting.py
@@ -7,7 +7,7 @@ def clicked():
     window.update()
     window.deiconify()
 window = Tk()
-# window.attributes("-topmost", True)
+window.attributes("-topmost", True)
 window.title("I'm watching you and notify every " + str(period//60000) + " seconds!" )
 btn = Button(window, text="change sitting position", font=("Arial Bold", 50), command = clicked)
 btn.grid(column=0, row=0)
